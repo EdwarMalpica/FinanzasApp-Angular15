a23ef3d632d354ae00252983e13c82cd4b9cba84
EdwarMalpica <edwar.malpica@uptc.edu.co>
2023-05-23 23:53:26 -0500

    Implemtacion https

--- src/app/Services/api-service.service.ts
+++ src/app/Services/api-service.service.ts
@@ -5,37 +5,39 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiServiceService {
-  api = 'http://127.0.0.1:5000/';
+  api = 'https://mongoproject.alwaysdata.net/';
 
   constructor(private http: HttpClient) {}
 
   obtenerDatos() {
-    return this.http.get(this.api+'history');
+    return this.http.get(this.api + 'history');
   }
 
-  delete(id:string){
-    this.http.delete(this.api+'delete/'+id).subscribe( ()=>{
-      console.log('Eliminado exitosamente');
-    }, error=>{
-      console.error(error);
-    });
+  delete(id: string) {
+    this.http.delete(this.api + 'delete/' + id).subscribe(
+      () => {
+        console.log('Eliminado exitosamente');
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
-  agregar(type:string,descripcion:string, value:number){
+  agregar(type: string, descripcion: string, value: number) {
     const data = {
       type: type,
       total: value,
-      update_description:descripcion
+      update_description: descripcion,
     };
 
-    this.http.post(this.api+'finance',data).subscribe( ()=>{
-      console.log('Inserccion exitosa');
-
-    },error=>{
-      console.error(error);
-
-    })
-
+    this.http.post(this.api + 'finance', data).subscribe(
+      () => {
+        console.log('Inserccion exitosa');
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
-
 }
